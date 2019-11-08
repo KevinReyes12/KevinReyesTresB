880809be17b60baeda36dddb863e8e559d164c45
KevinReyes12 <56658182+KevinReyes12@users.noreply.github.com>
2019-11-07 23:02:22 -0500

    Add files via upload

---
+++ KevinReyes3B/TADCola/Program.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TADCola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+        }
+        Object[] ArregloCola;
+        int primero = -1, ultimo = -1, Num_elem;
+        Object[] ArregloColaaux;
+        int primeroaux = -1, ultimoaux = -1;
+        int Cant_elem = 5;
+
+        
+        
+
+        //Encola un elemento
+        public void Ingresar(Object x)
+        {
+            if (VaciaCola())
+            { //si mi cola esta vacia
+                ultimo++;  //ese dato es el ultimo
+                primero++;  //ese dato sera el primero
+                ArregloCola[ultimo] = x;   //guardo el dato x en el
+            }                               //ArregloCola en la posicion de ultimo
+            else
+            {  //si hay datos
+
+                if (ultimo == Cant_elem - 1) //esta lleno mi cola
+                    Console.WriteLine("No hay campo"); 
+
+            else
+                {
+                    ultimo++; //pasa a ser el ultimo por eso incrementa
+                    ArregloCola[ultimo] = x; //guardo el dato X
+
+                }
+            }
+        }
+        public void Ingresaraux(Object x)
+        {
+            if (VaciaCola())
+            { //si mi cola esta vacia
+                ultimoaux++;  //ese dato es el ultimo
+                primeroaux++;  //ese dato sera el primero
+                ArregloColaaux[ultimoaux] = x;   //guardo el dato x en el
+            }                               //ArregloCola en la posicion de ultimo
+            else
+            {  //si hay datos
+
+                if (ultimoaux == Cant_elem - 1) //esta lleno mi cola
+                    Console.WriteLine("No hay campo");
+
+            else
+                {
+                    ultimoaux++; //pasa a ser el ultimo por eso incrementa
+                    ArregloColaaux[ultimoaux] = x; //guardo el dato X
+
+                }
+            }
+        }
+        public Object Avanzar()
+        {
+            Object data = null;
+            if (VaciaCola()) //si la cola esta vacia
+                Console.WriteLine("No hay Elementos"); 
+
+        else
+            { //si hay datos
+                data = ArregloCola[primero]; //hago una copia del dato que sale
+                if (primero == ultimo)
+                { //solo hay un dato
+                    primero = -1;  //cola queda vacia
+                    ultimo = -1;   // cola queda vacia
+                }
+                else
+                {  //hay mas de un dato
+                    primero++;
+                }
+
+            }
+            return (data);  //retorno el dato que sale de la cola
+        }
+
+        //Desencola un elemento
+        public Object Avanzaraux()
+        {
+            Object data = null;
+            if (VaciaColaaux()) 
+                Console.WriteLine("No hay Elementos"); 
+
+        else
+            { //si hay datos
+                data = ArregloColaaux[primeroaux]; 
+                if (primeroaux == ultimoaux)
+                { //solo hay un dato
+                    primeroaux = -1;  
+                    ultimoaux = -1;   
+                }
+                else
+                {  //hay mas de un dato
+                    primeroaux++;
+                }
+
+            }
+            return (data);  //retorno el dato que sale de la cola
+        }
+
+
+        //Retorna si esta vacia la cola
+        public Boolean VaciaCola()
+        {
+            if ((ultimo == -1) && (primero == -1))
+                return (true);  //cola vacia
+            else
+                return (false);  //cola tiene datos
+        }
+        public Boolean VaciaColaaux()
+        {
+            if ((ultimoaux == -1) && (primeroaux == -1))
+                return (true);  //cola vacia
+            else
+                return (false);  //cola tiene datos
+        }
+        public void Imprimir()
+        {
+            Object desplazarse2;
+            if (VaciaCola())
+            {
+                Console.WriteLine("No hay elementos"); 
+            }
+            else
+            {
+                Console.WriteLine("La Cola es: "); 
+                while (!VaciaCola())
+                {
+                    desplazarse2 = Avanzar();
+                    Ingresaraux(desplazarse2);
+                } while (!VaciaColaaux())
+                {
+                    desplazarse2 = Avanzaraux();
+                    Console.WriteLine(desplazarse2 + "\t"); 
+                    Ingresar(desplazarse2);
+
+
+
+                }
+                Console.Write(""); 
+            }
+        }
+        public int contar()
+        {
+            Object desplazarse2;
+            int contador = 0;
+            if (VaciaCola())
+            {
+                Console.WriteLine("No hay elementos"); 
+            }
+            else
+            {
+                //System.out.print("La Cola es: ");
+                while (!VaciaCola())
+                {
+                    desplazarse2 = Avanzar();
+                    contador = contador + 1;
+                    Ingresaraux(desplazarse2);
+                } while (!VaciaColaaux())
+                {
+                    desplazarse2 = Avanzaraux();
+                    //System.out.println(desplazarse2+"\t");
+                    Ingresar(desplazarse2);
+
+
+
+                }
+                //System.out.print("");
+            }
+            return (contador);
+        }
+
+
+    }
+    
+}
---
+++ KevinReyes3B/TADPila/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TADPila
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            Menu();
+        }
+        public static void Menu()
+        {
+            Console.WriteLine("******** Menu de TAD PILA ********");
+            Console.WriteLine("1.Insertar dato en pila");
+            Console.WriteLine("2.Eliminar dato en pila");
+            Console.WriteLine("3.Imprimir pila");
+            int opcion = int.Parse(Console.ReadLine());
+            Opciones(opcion);
+        }
+        public static void Opciones(int op){
+            switch (op)
+            {
+                case 1:Insertar();
+                    break;
+                case 2:Eliminar();
+                    break;
+                case 3:Imprimir();
+                    break;
+
+               default:Menu();
+                    break;
+            }
+        }
+        static String [] Pila=new String[5];
+        static int cima = -1;
+        public static void Insertar()
+        {
+            Console.WriteLine("Ingrese un dato a la Pila");
+            String dato = Console.ReadLine();
+            Crear(dato);
+        }
+        public static void Crear(String dato)
+        {
+            if ((Pila.Length-1)==cima)
+            {
+                Console.WriteLine("Capacidad de la pila al límite");
+                Imprimir();
+            }
+            else
+            {
+                cima++;
+            }
+            Agregar(dato);
+        }
+        public static void Agregar(String dato)
+        {
+            Pila[cima] = dato;
+            Menu();
+        }
+        public static void Eliminar()
+        {
+            if (cima==-1)
+            {
+                Console.WriteLine("la pila esta vacíá");
+            }
+            else
+            {
+                Pila[cima] = null;
+                --cima;
+            }
+            Menu();
+        }
+        public static void Imprimir()
+        {
+            for (int i = Pila.Length-1; i >=0 ; i--)
+            {
+                Console.WriteLine(Pila[i]);
+            }
+            Menu();
+        }
+        
+    }
+}
